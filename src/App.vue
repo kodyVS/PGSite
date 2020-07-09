efa24ff9b93164287d0a9f945aeb32eec6bf067e
knox <59428387+kodyVS@users.noreply.github.com>
2020-07-09 16:22:34 -0600

    test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <h1>PG-Site</h1>
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <img alt="Vue logo" src="./assets/logo.png">-->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <NavBar />
     <router-view />
